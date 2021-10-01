fc176266ac5035974af038d3354ae9d7c289437f
LALIT LOHANI <lalitlohani01@gmail.com>
2021-10-01 22:13:17 +0530

    Updates

--- app.js
+++ app.js
@@ -32,6 +32,6 @@ app.post('/', (req, res) => {
   })
 });
 
+const port = process.env.PORT || 3000;
 
-
-app.listen(process.env.PORT || 3000);
+app.listen(port, () => console.log("Port running in " + port));
